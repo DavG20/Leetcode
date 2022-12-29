1bd3dcee8fed9cf6e5740c3e654ca7678138422d
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2022-12-29 17:59:36 +0300

    Time: 1880 ms (94.66%), Space: 68.7 MB (78.49%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.py
@@ -7,14 +7,21 @@ class Solution:
         
         for player in matches:
             loser.append(player[1])
+        
+        #lets change loser to set it will help us not retriev 
+        #every time to check if winner is in or not in loser
+        #time complexity to change list to set is O(n)
         loser_set=set(loser)
+        
         for player in matches:
+            #in here in oprator time complexity is O(1)
             if player[0] not in loser_set:
                 winner.add(player[0])
         
-        
+        #lets get the freq of loser since we want the one time losers only
         count_loser=Counter(loser)
         
+        #again lets use set , it will help us not to check if loser is already in our lis or not
         losers_set_final=set()
         
         for losers in count_loser.items():
