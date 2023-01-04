d6b0d501ab8a425f87f139a6debfc3d0ade97db6
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-01-04 19:23:25 +0300

    Time: 2614 ms (8.03%), Space: 58 MB (5.16%) - LeetHub

--- 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
+++ 2109-adding-spaces-to-a-string/2109-adding-spaces-to-a-string.py
@@ -1,18 +1,32 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
         
-        answer=[]
+        
+        modified_string=[]
+        
+        len_word=len(s)
+        
+        #change space to set for the purpose of time complexity
+        space_set=set(spaces)
+        
+        for i in range(len_word):
+            if i in space_set:
+                modified_string.append(" ")
+            modified_string.append(s[i])
+        return "".join(modified_string)
+        
+#         answer=[]
         
         
-        answer.append(s[:spaces[0]])
+#         answer.append(s[:spaces[0]])
                 
-        index=1
+#         index=1
         
-        while index<len(spaces):
-            answer.append(s[spaces[index-1]:spaces[index]])
-            index+=1
+#         while index<len(spaces):
+#             answer.append(s[spaces[index-1]:spaces[index]])
+#             index+=1
             
-        answer.append(s[spaces[index-1]:])
+#         answer.append(s[spaces[index-1]:])
         
-        string_with_space=" ".join(answer)
-        return string_with_space
+#         string_with_space=" ".join(answer)
+#         return string_with_space
