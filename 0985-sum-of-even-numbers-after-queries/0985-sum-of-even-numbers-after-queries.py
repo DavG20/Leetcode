bd9cc7aac4a1969deb02b0bfd977000c143dd8c9
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-01-08 20:45:53 +0300

    Time: 538 ms (73.98%), Space: 20.8 MB (6.75%) - LeetHub

--- 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
+++ 0985-sum-of-even-numbers-after-queries/0985-sum-of-even-numbers-after-queries.py
@@ -1,17 +1,42 @@
 class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        even_sum=0
+        pre_even_sum=0
+        
+        
+        #get the intial even sum
+        
         for num in nums:
             if num%2==0:
-                even_sum+=num     
-        result=[]
-        for i in range(len(nums)):
-            if nums[queries[i][1]]%2==0:
-                even_sum-=nums[queries[i][1]]
-            nums[queries[i][1]]=nums[queries[i][1]]+queries[i][0]
-            if nums[queries[i][1]]%2==0:
-                even_sum+=nums[queries[i][1]]
-                result.append(even_sum)
+                pre_even_sum+=num
+        
+        new_nums=nums[:]
+        
+        answer=[]
+        
+        for query in queries:
+            if new_nums[query[1]]%2==0:
+                pre_even_sum-=new_nums[query[1]]
+                modified_val=new_nums[query[1]]+query[0]
+                if modified_val%2==0:
+                    new_nums[query[1]]=modified_val
+                    pre_even_sum+=modified_val
+                    answer.append(pre_even_sum)
+                else:
+                    new_nums[query[1]]=modified_val
+                    answer.append(pre_even_sum)
+                    
             else:
-                result.append(even_sum)
-        return result
+                modified_val=new_nums[query[1]]+query[0]
+                if modified_val%2==0:
+                    new_nums[query[1]]=modified_val
+                    pre_even_sum+=modified_val
+                    answer.append(pre_even_sum)
+                else:
+                    new_nums[query[1]]=modified_val
+                    answer.append(pre_even_sum)
+        return answer
+
+                
+                    
+            
+                
