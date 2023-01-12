da9d6735430abb9f977fa431dd02f1b79ee05d08
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-01-12 18:50:10 +0300

    Time: 33 ms (92.05%), Space: 13.8 MB (98.64%) - LeetHub

---
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -0,0 +1,7 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        answer=0
+        for i in range(1,n+1):
+            answer=(answer+k)%i
+        return answer+1
+    
