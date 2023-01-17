4fbaf418e3b0a48a43ea9d4aa6e7c7c8ff013c84
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-01-17 18:35:27 +0300

    Time: 1773 ms (5.03%), Space: 14.3 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -4,7 +4,7 @@ class Solution:
         isSwap=True
         
         for i in range(len(names)):
-            for j in range(len(names)-1):
+            for j in range(len(names)-1-i):
                 if heights[j]<heights[j+1]:
                     heights[j],heights[j+1]=heights[j+1],heights[j]
                     names[j],names[j+1]=names[j+1], names[j]
