61623a3a17a375fd655d64190686dd5ab4938a25
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-01-29 21:06:50 +0300

    Time: 37 ms (52.12%), Space: 13.8 MB (77.76%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -8,17 +8,17 @@ class Solution:
             answer+=matrix.pop(0)
             
             if matrix and matrix[0]:
+                
                 for array in matrix:
+                    
                     answer.append(array.pop())
+            
             if matrix:
-                #reverse the array last andd add
+                
                 answer+=matrix.pop()[::-1]
                 
             if matrix and matrix[0]:
                 
-                for arr in matrix[::-1]:
-                    answer.append(arr.pop(0))
-        return(answer)
-        
-        
-                
+                for array in matrix[::-1]: 
+                    answer.append(array.pop(0))
+        return answer
