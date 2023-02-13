bed2f9ddd54c44e168f74bca78deed599bbc2132
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-02-13 10:16:17 +0300

    Time: 171 ms (79.48%), Space: 25.2 MB (17.60%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        remainder = k%n
+        nums[:] = nums[n-remainder:] + nums[:n-remainder]
+        return nums
+        
+        
+        
