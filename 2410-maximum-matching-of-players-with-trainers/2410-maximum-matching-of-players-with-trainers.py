6d3baecd9d059448bf27190e7039d17c6738aeb9
Dawit Teshager <77918544+DavG20@users.noreply.github.com>
2023-02-15 15:45:40 +0300

    Update 2410-maximum-matching-of-players-with-trainers.py

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -1,17 +1,26 @@
 class Solution:
     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
-        count=0
+       
         players.sort()
+        
         trainers.sort()
-        j=0
-        for num in players:
-            while j<len(trainers):
-                if num<=trainers[j]:
-                    count+=1
-                    j+=1
-                    break
-                else:
-                    j+=1
-        return count
+        
+        left = 0 
+        
+        right = 0 
+        
+        count = 0
+        
+        while left < len(players) and right < len(trainers):
+            if players[left] <= trainers[right]:
+                count += 1
+                
+                left += 1
+                
+                right += 1
+                
+            else :
+                right += 1
+        return count 
             
         
